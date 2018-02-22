c22335e7fd057c18ece7afbb319be0efaa6e97d9
strengthening <ducg@foxmail.com>
2018-02-22 08:38:37 +0800

    add

--- marketFutureAPI/okexAPI/marketFutureAPI.js
+++ marketFutureAPI/okexAPI/marketFutureAPI.js
@@ -166,7 +166,6 @@ class OkexMarketFutureAPI {
                 try {
                     let dataStr = await http
                         .postPromiseReq(this._config.base_url + '/api/v1/future_trades_history.do', param);
-                    console.log(dataStr);
                     let dataObj = JSON.parse(dataStr);
                     if (dataObj.length && dataObj.length > 0) {
                         return resolve([dataObj, undefined]);
